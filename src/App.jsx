e3674b1e34d3fb5222805c12cdf4c3d45a3a9ebc
Facundo Saavedra <facusaavedrx@gmail.com>
2023-01-27 10:52:37 -0300

    Se agrego componente formulario y header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import styled from '@emotion/styled'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+
+const Contenedor = styled.main`
+  max-width: 600px;
+  margin: 0 auto;
+`
+const ContenedorFormulario = styled.article`
+  background-color: #FFF;
+  padding: 3rem;
+`
 function App () {
   return (
-    <div className='App'>
-      <h1>Hola mundo desde App</h1>
-      <h2>Como andan</h2>
-    </div>
-
+    <Contenedor>
+      <Header
+        titulo='Hola mundo'
+      />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   )
 }
 
