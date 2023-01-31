04268525219fe36f4fca88d1324b52abbf3added
Facundo Saavedra <facusaavedrx@gmail.com>
2023-01-31 16:56:36 -0300

    Se agrego funcion primerMayuscula y estilado a componente resumen

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,15 +1,25 @@
+import styled from '@emotion/styled'
+import { primerMayuscula } from '../helper'
+
+const ContenedorResumen = styled.section`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838F;
+  color: #FFF;
+  margin-top: 1rem;
+`
 function Resumen ({ datos }) {
   const { marca, year, plan } = datos
   if (marca === '' || year === '' || plan === '') return null
   return (
-    <>
+    <ContenedorResumen>
       <h2> Resumen de cotizacion </h2>
       <ul>
-        <li>Marca: </li>
-        <li>Plan: </li>
-        <li>Año del auto: </li>
+        <li>Marca: {primerMayuscula(marca)} </li>
+        <li>Plan: {primerMayuscula(plan)} </li>
+        <li>Año del auto: {year} </li>
       </ul>
-    </>
+    </ContenedorResumen>
   )
 }
 export default Resumen
